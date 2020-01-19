14fa2fc78d2ae522752568f2c9e02109ad6dc1c3
Matt Archer <matt.archer@digital.hmrc.gov.uk>
2020-01-19 18:13:18 +0000

    types

--- questions/indicies.js
+++ questions/indicies.js
@@ -3,23 +3,23 @@ function getIndiciesQuestions() {
     var questions = [];
 
     for (i = 1; i <= 14; i++) {
-        questions.push({question : i + "<sup>2</sup>", answer: i * i});
-        questions.push({question : i + " x " + i, answer: i * i});
+        questions.push({type : 1, question : i + "<sup>2</sup>", answer: i * i});
+        questions.push({type : 2, question : i + " x " + i, answer: i * i});
     }
 
     for (i = 1; i <= 5; i++) {
-        questions.push({question : i + "<sup>3</sup>", answer: i * i * i});
-        questions.push({question : i + " x " + i + " x " + i, answer: i * i * i});
+        questions.push({type : 3, question : i + "<sup>3</sup>", answer: i * i * i});
+        questions.push({type : 4, question : i + " x " + i + " x " + i, answer: i * i * i});
     }
 
     for (i = 1; i <= 3; i++) {
-        questions.push({question : i + "<sup>4</sup>", answer: i * i * i * i});
-        questions.push({question : i + " x " + i + " x " + i  + " x " + i, answer: i * i * i * i});
+        questions.push({type : 5, question : i + "<sup>4</sup>", answer: i * i * i * i});
+        questions.push({type : 6, question : i + " x " + i + " x " + i  + " x " + i, answer: i * i * i * i});
     }
 
     for (i = 1; i <= 3; i++) {
-        questions.push({question : i + "<sup>5</sup>", answer: i * i * i * i * i});
-        questions.push({question : i + " x " + i + " x " + i  + " x " + i  + " x " + i, answer: i * i * i * i * i});
+        questions.push({type : 7, question : i + "<sup>5</sup>", answer: i * i * i * i * i});
+        questions.push({type : 8, question : i + " x " + i + " x " + i  + " x " + i  + " x " + i, answer: i * i * i * i * i});
     }
 
     return questions;
--- questions/percentages.js
+++ questions/percentages.js
@@ -5,30 +5,30 @@ function getPercentageQuestions() {
 
     for (i = 10; i <= 120; i=i+10) {
         for (p = 0; p <= 100; p=p+10) {
-        questions.push({question : p + "% of " + i, answer: i * (p / 100)});
+        questions.push({type : 1, question : p + "% of " + i, answer: i * (p / 100)});
         }
     }
 
     for (i = 20; i <= 120; i=i+20) {
         for (p = 5; p <= 95; p=p+10) {
-        questions.push({question : p + "% of " + i, answer: i * (p / 100)});
+        questions.push({type : 2, question : p + "% of " + i, answer: i * (p / 100)});
         }
     }
 
     for (i = 4; i <= 48; i=i+4) {
-        questions.push({question : "25% of " + i, answer: i / 4});
+        questions.push({type : 3, question : "25% of " + i, answer: i / 4});
     }
 
     for (i = 1; i <= 19; i=i+2) {
-        questions.push({question : "50% of " + i, answer: i / 2});
+        questions.push({type : 4, question : "50% of " + i, answer: i / 2});
     }
 
     for (i = 1; i <= 100; i=i+Math.floor(Math.random() * 10)) {
-        questions.push({question : "10% of &pound;" + i, answer: "&pound;" + (i / 10).toFixed(2)});
+        questions.push({type : 5, question : "10% of &pound;" + i, answer: "&pound;" + (i / 10).toFixed(2)});
     }
 
     for (i = 1; i <= 100; i=i+Math.floor(Math.random() * 10)) {
-        questions.push({question : "1% of &pound;" + i, answer: "&pound;" + (i / 100).toFixed(2)});
+        questions.push({type : 6, question : "1% of &pound;" + i, answer: "&pound;" + (i / 100).toFixed(2)});
     }
 
     return questions;
