0833387ea09e03e3e8ce23b9177abfc796ad4e81
Matt Archer <matt.archer@digital.hmrc.gov.uk>
2020-01-21 08:59:55 +0000

    prime

---
+++ questions/prime.js
@@ -0,0 +1,45 @@
+function getPrimeQuestions() {
+
+    var questions = [];
+
+    for (i = 1; i <= 2; i++) {
+        if (i == 1) {
+            questions.push({type : 1, question : i + " is a prime number?", answer: false, prompt: "The definition of a <B>prime number</B> is a positive integer that has exactly two positive divisors. However, <B>1</B> only has <B>one</B> positive divisor (1 itself), so it is <B>not</B> prime."});
+        }
+        else if (test_prime(i)) {
+            questions.push({type : 1, question : i + " is a prime number?", answer: true, prompt: i + " is a prime number."});
+        } else {
+            questions.push({type : 1, question : i + " is a prime number?", answer: false, prompt: i + " can be divided by " + factors(i)});
+        }
+    }
+
+    return questions;
+
+}
+
+const factors = number => Array
+    .from(Array(number + 1), (_, i) => i)
+    .filter(i => number % i === 0)
+
+function test_prime(n)
+{
+
+  if (n===1)
+  {
+    return false;
+  }
+  else if(n === 2)
+  {
+    return true;
+  }else
+  {
+    for(var x = 2; x < n; x++)
+    {
+      if(n % x === 0)
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+}
