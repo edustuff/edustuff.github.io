963e1e24dee1b53392291a424ff5883f02334b10
Matt Archer <matt.archer@digital.hmrc.gov.uk>
2020-03-08 22:06:50 +0000

    machine

--- questions/number-machines.js
+++ questions/number-machines.js
@@ -1,27 +1,129 @@
+var id = 0;
+
 function getNumberMachines() {
 
     var questions = [];
 
+    var graphType = "graph LR;";
 
+    var graphFormatting = "classDef edgePath stroke:#1899DA,stroke-width:4px;classDef node stroke-width:4px,fill:#F08B25,stroke:#1899DA;";
 
-    for (i = 1; i<50; i++) {
+    for (i = 1; i<1000; i++) {
 
-//      padding-top:3vh;
-//      padding-bottom:3vh;
-//      border-radius: 3vw;
-//      background: rgb(240, 139, 37, 1);
-//      background: linear-gradient(45deg, rgba(240, 139, 37, 1) 0%, rgba(229, 63, 50, 1) 100%);
-//      border: 1vw solid rgba(24, 153, 218, 1);
-//      font-size: 5vw;
-//      text-align: center;
+        for (x = 0; x <2; x++) {
 
-        var question = "graph LR;A[Client] --> B[Load Balancer " + i + "];classDef edgePath stroke:#1899DA,stroke-width:4px;classDef node stroke-width:4px,fill:#F08B25,stroke:#1899DA;";
+            id=0;
 
-        questions.push({type : 1, question : question, answer: i});
+            var startingNumber = round(Math.random() * 144, 0);
+
+            var question = graphType;
+
+            if (x == 0) {
+                question = question + step(startingNumber);
+            } else {
+                question = question + step("?");
+            }
+
+            var nextStepNumber = startingNumber;
+
+            var transitionOk = true;
+
+            var types = [];
+            var stepResults = [];
+
+            for (n = 1; n <=3; n++) {
+
+                stepResults.push(nextStepNumber);
+
+                var nextStep1 = nextStep(startingNumber, nextStepNumber);
+
+                nextStepText = nextStep1.text;
+
+                types.push(nextStep1.option);
+
+                question = question + " --> " + nextStepText;
+
+                var nextNumberStr  = round(nextStep1.number,1) + "";
+
+                if (nextStepNumber == 0 || nextStepNumber == 1 ||   stepResults.includes(nextStep1.number) || nextStep1.number > 144 || round(nextStep1.number,1) != nextStep1.number || (round(nextStep1.number,0) != nextStep1.number && nextNumberStr[nextNumberStr.length-1] != "5")) {
+                    transitionOk = false;
+                }
+
+                nextStepNumber = nextStep1.number;
+            }
+
+            if (x == 0) {
+                question = question + " --> " + step("?");
+                answer = nextStepNumber;
+            } else {
+                question = question + " --> " + step(nextStepNumber);
+                answer = startingNumber;
+            }
+
+            question = question + ";";
+
+            question = question + graphFormatting;
+
+            if (transitionOk) {
+                for (t=0; t<types.length; t++) {
+                    questions.push({type : types[t], question : question, answer: answer, prompt: "Answer = " + answer});
+                }
+            }
+
+        }
 
     }
 
     return questions;
+
+}
+
+function step(text) {
+    return nextId() + "[" + text + "]";
+}
+
+function nextId() {
+    id=id+1;
+    return "A" + id;
+}
+
+function nextStep(originalNumber, number) {
+
+    var adjustment = round(Math.random() * 12, 0) + 1;
+
+    var option = round(Math.random() * 8, 0) + 1;
+
+    switch(option) {
+        case 1:
+            return {option: option, number: number + adjustment, text: step("add " + adjustment)};
+            break;
+        case 2:
+            return {option: option, number: number - adjustment, text: step("subtract " + adjustment)};
+            break;
+        case 3:
+            return {option: option, number: number * adjustment, text: step("multiply by " + adjustment)};
+            break;
+        case 4:
+            return {option: option, number: number / adjustment, text: step("divide by " + adjustment)};
+            break;
+        case 5:
+            return {option: option, number: number / 2, text: step("halve it")};
+            break;
+        case 6:
+            return {option: option, number: number * 2, text: step("double it")};
+            break;
+        case 7:
+            return {option: option, number: number * number, text: step("square it")};
+            break;
+        case 8:
+            return {option: option, number: number / originalNumber, text: step("divide by original number")};
+            break;
+        case 9:
+            return {option: option, number: number + (adjustment * originalNumber), text: step("add " + adjustment + " times original number")};
+            break;
+    }
+
+    throw("Unexpected State Exception");
 
 }
 
